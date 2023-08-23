609377bee96c8c21706f1d4795e0cca2011c8dcd
kavalcio <sarpkavalcioglu@gmail.com>
2023-08-22 23:39:19 -0400

    Add tab navigation

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,21 +1,59 @@
-import { CMS_NAME } from '../lib/constants'
+import { useRouter } from 'next/router';
+import clsx from 'clsx';
+import Link from 'next/link'
+
+const tabs = [
+  {
+    name: 'Film/TV',
+    url: '/film',
+  },
+  {
+    name: 'Conceptual Designs',
+    url: '/designs',
+  },
+  {
+    name: 'Sewing',
+    url: '/sewing',
+  },
+  {
+    name: 'Resume',
+    url: '/resume',
+  },
+  {
+    name: 'Contact',
+    url: '/contact',
+  },
+];
 
 const Intro = () => {
+  const { pathname } = useRouter();
   return (
-    <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
+    // <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
+    <section style={{
+      display: 'flex',
+      justifyContent: 'space-between',
+      marginTop: 64,
+      marginBottom: 64,
+    }}>
+      <h1 className="text-5xl font-bold tracking-tighter leading-tight md:pr-8">
         Charlotte Claytor
       </h1>
-      <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        A statically generated blog example using{' '}
-        <a
-          href="https://nextjs.org/"
-          className="underline hover:text-blue-600 duration-200 transition-colors"
-        >
-          Next.js
-        </a>{' '}
-        and {CMS_NAME}.
-      </h4>
+      <section style={{
+        display: 'flex',
+        
+      }}>
+        {tabs.map((tab, index) => (
+          <Link
+            key={index}
+            as={tab.url}
+            href={tab.url}
+            aria-label={tab.name}
+            className={clsx('navigation-tab', pathname === tab.url && 'underlined')}
+          >
+            {tab.name}
+          </Link>
+        ))}
+      </section>
     </section>
   )
 }
--- components/post-preview.tsx
+++ components/post-preview.tsx
@@ -25,8 +25,17 @@ const PostPreview = ({
     <div>
       <div className="mb-5">
         <CoverImage slug={slug} title={title} src={coverImage} />
+        <h3 className="text-3xl mb-3 leading-snug">
+          <Link
+            as={`/posts/${slug}`}
+            href="/posts/[slug]"
+            className="hover:underline"
+          >
+            {title}
+          </Link>
+        </h3>
       </div>
-      <h3 className="text-3xl mb-3 leading-snug">
+      {/* <h3 className="text-3xl mb-3 leading-snug">
         <Link
           as={`/posts/${slug}`}
           href="/posts/[slug]"
@@ -39,7 +48,7 @@ const PostPreview = ({
         <DateFormatter dateString={date} />
       </div>
       <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
-      <Avatar name={author.name} picture={author.picture} />
+      <Avatar name={author.name} picture={author.picture} /> */}
     </div>
   )
 }
