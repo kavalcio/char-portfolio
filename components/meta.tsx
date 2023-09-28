1cb423a7994f8edc4b2402ad276e89611f876bcc
kavalcio <sarpkavalcioglu@gmail.com>
2023-09-28 16:48:03 -0400

    Meta tag description

--- components/meta.tsx
+++ components/meta.tsx
@@ -27,7 +27,7 @@ const Meta = () => {
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
       <meta
         name="description"
-        content={`A statically generated blog example using Next.js and ${CMS_NAME}.`}
+        content={`Portfolio website for Charlotte Claytor.`}
       />
       <meta property="og:image" content={HOME_OG_IMAGE_URL} />
     </Head>
