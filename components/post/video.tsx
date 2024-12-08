7416ccd0c13339f91856a76209bc304923872627
kavalcio <sarpkavalcioglu@gmail.com>
2024-12-08 00:04:38 -0500

    Attempt to fix deployment

--- components/post/video.tsx
+++ components/post/video.tsx
@@ -5,7 +5,7 @@ type Props = {
 const Video = ({ source }: Props) => {
   return (
     <div style={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
-      <iframe width="560" height="315" src={source} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+      <iframe width="560" height="315" src={source} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
     </div>
   );
 }
